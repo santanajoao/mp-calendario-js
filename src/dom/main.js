9900dfecce2ee6385de0539192fde9605b1ef8fe
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-05 23:25:08 -0300

    feat: adiciona listeners e inicializações

---
+++ src/dom/main.js
@@ -0,0 +1,28 @@
+import '../style.css';
+import '../styles/calendar.css';
+import { renderLanguageOptions } from './language';
+import { renderCalendar, updateLocale, updateCalendar } from './calendar';
+import { languageSelect, prevMonthButton, nextMonthButton } from './elements';
+
+const currentDate = new Date();
+let selectedLocale = new Intl.DateTimeFormat().resolvedOptions().locale;
+
+languageSelect.addEventListener('change', () => {
+  selectedLocale = languageSelect.value;
+  updateLocale(currentDate, selectedLocale);
+});
+
+prevMonthButton.addEventListener('click', () => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  updateCalendar(currentDate, selectedLocale);
+});
+
+nextMonthButton.addEventListener('click', () => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  updateCalendar(currentDate, selectedLocale);
+});
+
+renderLanguageOptions();
+languageSelect.value = selectedLocale;
+
+renderCalendar(currentDate, selectedLocale);
