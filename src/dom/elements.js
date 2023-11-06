ff2c6e4a68356c0afe4e85cf30c5eadca7aae32a
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-11-05 23:17:04 -0300

    build: cria arquivo com elementos do dom

---
+++ src/dom/elements.js
@@ -0,0 +1,7 @@
+export const weekDayNamesContainer = document.querySelector('.calendar__day-names');
+export const calendarMonth = document.querySelector('.calendar__month');
+export const calendarYear = document.querySelector('.calendar__year');
+export const monthDaysContainer = document.querySelector('.calendar__days-grid');
+export const languageSelect = document.querySelector('.language-select');
+export const prevMonthButton = document.querySelector('#prev-month-button');
+export const nextMonthButton = document.querySelector('#next-month-button');
